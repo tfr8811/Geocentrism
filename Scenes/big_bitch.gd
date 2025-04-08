dc4bd2b574f5bbbd35b093b986f51a1ad05e6573
Thomas Roman <tfr8811@g.rit.edu>
2025-04-08 15:41:14 -0400

    Explosions sped up and momentum conserved

--- Scenes/big_bitch.gd
+++ Scenes/big_bitch.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+extends CharacterBody2D
 var speed = 100
 var health = 1
 var dead = false
@@ -9,12 +9,17 @@ var launchTime = 0.0
 func _process(delta: float) -> void:
 	if launchTime > 0.0: launchTime -= delta
 func _physics_process(delta: float) -> void:
-	# we want it to bounce a bit when damaged
-	if (is_instance_valid(GlobalWorldState.Player) && invincibility <= 0):
+	# death handler
+	if dead:
+		# keep momentum
+		move_and_slide()
+		return
+	if (is_instance_valid(GlobalWorldState.Player)):
 		var player = GlobalWorldState.Player
 		var direction = player.global_position - global_position
 		direction = direction.normalized()
-		if launchTime <= 0: linear_velocity = lerp(linear_velocity, direction * speed, delta * 5.0)
+		if launchTime <= 0: velocity = lerp(velocity, direction * speed, delta * 5.0)
+		move_and_slide()
 	# reduce invincibility, this prevents double hits
 	if invincibility > 0:
 		invincibility -= delta
@@ -32,7 +37,7 @@ func take_damage():
 		sprite.play("death")
 		hitbox.disabled = true
 func launch(direction: Vector2):
-	linear_velocity = direction * speed * 5
+	velocity = direction * speed * 5
 	launchTime = 0.25
 
 
--- Scenes/demon.gd
+++ Scenes/demon.gd
@@ -11,6 +11,8 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	# death handler
 	if dead:
+		# keep momentum
+		move_and_slide()
 		return
 	if (is_instance_valid(GlobalWorldState.Player)):
 		var player = GlobalWorldState.Player
