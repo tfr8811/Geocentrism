4b925e3a13b74270f3d88938802e740815acbd47
Thomas Roman <tfr8811@g.rit.edu>
2025-06-27 14:59:27 -0400

    added a remap to make the sun physics more user friendly

--- Scenes/Characters/Allies/sun.gd
+++ Scenes/Characters/Allies/sun.gd
@@ -38,6 +38,8 @@ func _physics_process(delta: float) -> void:
 		else:
 			if faceFreeze <= 0:
 				faceAnim.set_animation("default")
+		# remap distance to make sun easier to fling around
+		distance = remap(distance, 170, 600, 200, 400)
 		# assuming G = 30000 and the masses are equal
 		var strength = 30000*mass*mass / (distance * distance);
 		force = force.normalized()
